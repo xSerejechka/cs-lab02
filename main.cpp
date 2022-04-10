b28fa0c2f978060872948507686ab5f4a3404c4f
Alice (GarkinSV) <GarkinSV@mpei.ru>
2022-04-10 15:33:42 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';
 }
