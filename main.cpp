896ff9cdf63499044c8d23406497bf5fc5092786
Alice (GarkinSV) <GarkinSV@mpei.ru>
2022-04-10 17:42:50 +0300

    code: double

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
